a966d2858874e98dc74ce7cf9f572bb3e7e0061b
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-29 13:31:21 -0600

    feat: update auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,15 +6,15 @@ export default class AuthService extends HTTPService {
     const body = { email: email, password: password, areaId:areaId };
     const json = await this.post("auth/login", body);
     console.log("Token plano recibido en AuthService.signIn:", json);
-    return new Token(json.token);
+    return Token.fromJson(json.token);
   }
 
-  async signUp(username: string, newemail: string, newpassword: string, areId: string) {
-    if (!username || !newpassword || !newemail || !areId) {
+  async signUp(username: string, newemail: string, newpassword: string, areaId: string) {
+    if (!username || !newpassword || !newemail || !areaId) {
       throw new Error("Username, email and password are required");
     }
 
-    const body = { name: username, email: newemail, password: newpassword, areId: areId};
+    const body = { name: username, email: newemail, password: newpassword, areaId: areaId};
     const json = await this.post("auth/register", body);
     console.log("Token recibido en AuthService.signup:", json.token);
     return Token.fromJson(json);
