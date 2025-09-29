a4d3901d6a8cc7635c5fd50cbc73ec2636d4a099
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-29 12:22:10 -0600

    feat: add area service

--- src/services/area.service.ts
+++ src/services/area.service.ts
@@ -12,4 +12,33 @@ export default class AreaService extends HTTPService {
     const data = await super.get(this.path);
     return data.map((item: any) => Area.fromJson(item));
   }
+
+  async getById(id: string) {
+    const item = await super.get(`${this.path}/${id}`);
+    if (!item) return null;
+
+    return Area.fromJson(item);
+  }
+
+  async addArea(area: Area) {
+    //el body que se va mandar se construye
+    const body = Area.fromJsonModel(area).toJsonDTO();
+    //devuelte null o la area creada
+    const result = await super.post(`${this.path}`, body);
+    if (!result) return null;
+    //retorna la area creada es decir el resultado
+    return Area.fromJson(result);
+  }
+
+  async updateArea(id: string, area: Area) {
+    const body = Area.fromJsonModel(area).toJsonDTO();
+    const json = await super.put(`${this.path}/${id}`, body);
+    if (!json) return null;
+
+    return Area.fromJson(json);
+  }
+
+  async deleteArea(id: string) {
+    await super.delete(`${this.path}/${id}`);
+  }
 }
