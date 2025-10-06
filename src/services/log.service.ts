c60aa65fb2b8356081a16e1e23e5a196f4b98a36
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-05 18:57:40 -0600

    feat: add name for userid undefined

--- src/services/log.service.ts
+++ src/services/log.service.ts
@@ -11,12 +11,31 @@ export default class LogService extends HTTPService {
   async getLogs() {
     const response = await super.get(this.path);
     const data = response.data || [];
-    return data.map((item: any) => LogModel.fromJson(item));
+    return data
+      .map((item: any) => {
+        const log = LogModel.fromJson(item);
+
+        // Si el userId coincide, reemplazar por nombre
+        if (log.userId === "d976b2d4b8494815") {
+          log.userId = "Admin General";
+        }
+
+        return log;
+      })
+      .sort(
+        (a: LogModel, b: LogModel) =>
+          b.timestamp.getTime() - a.timestamp.getTime()
+      ); // de más reciente a más viejo
   }
 
   async getLogByArea(id: string) {
     const response = await super.get(`${this.path}/area/${id}`);
-    const data = response.data || []; // siempre aseguramos array
-    return data.map((item: any) => LogModel.fromJson(item));
+    const data = response.data || [];
+    return data
+      .map((item: any) => LogModel.fromJson(item))
+      .sort(
+        (a: LogModel, b: LogModel) =>
+          b.timestamp.getTime() - a.timestamp.getTime()
+      );
   }
 }
